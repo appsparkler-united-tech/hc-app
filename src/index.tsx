37f1b5c02dc38e7d71a23f70f0354bcf3f5790aa
Aakash Shah <appsparkler@gmail.com>
2023-10-30 10:16:45 +0530

    chore: wip

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const welcomeToHC = {
   popover: {
     title: "Welcome to HC App",
     description:
-      "We hope this app helps you discover your feelings and how they relate to your needs.  Please click next to continue.  And then click next to continue.",
+      "We hope this app helps you discover your feelings and how they relate to your needs.  Please click next to continue.",
   },
 };
 
@@ -26,7 +26,7 @@ const howAreYouStep = {
     "html body div#root div.MuiContainer-root.MuiContainer-maxWidthXl.css-141bmmb div.css-1v0xnyz div.MuiStepper-root.MuiStepper-horizontal.MuiStepper-alternativeLabel.css-1187icl div.MuiStep-root.MuiStep-horizontal.MuiStep-alternativeLabel.css-166ciyp span.MuiStepLabel-root.MuiStepLabel-horizontal.MuiStepLabel-alternativeLabel.css-1abj2s5",
   popover: {
     title: "How are you feeling?",
-    description: 'this is the first step.  Click "Next" to continue',
+    description: 'This is the first step.  Click "Next" to continue',
   },
 };
 
@@ -36,7 +36,7 @@ const thumbsUpOrDown = {
   popover: {
     title: "Thumbs up or down",
     description:
-      "click on thumbs-up if you're feeling good, positive, happy, excited or any of those uplifting feelings else thumbs down.",
+      "Click on thumbs-up if you're feeling good, positive, happy, excited or any of those uplifting feelings else click on thumbs down.",
   },
 };
 
@@ -46,7 +46,7 @@ const nextButtonToSelectFeelings = {
   popover: {
     title: "Go to the next step to select your feelings",
     description:
-      "If you have not selected thumbs-up or down; this button will be disabled.",
+      "If you have not selected thumbs-up or down; this button will be disabled. If it is enabled, click on it.Then click 'Next' to continue",
   },
 };
 
@@ -65,15 +65,17 @@ const feelingsList = {
   popover: {
     title: "List of feelings",
     description:
-      "you can select the feelings that closely match to what exactly you're feeling. Choose this feeling.",
+      "You can select one or more feelings that closely matches what you're feeling. Click on this feeling to select it.  Then click the 'Next' button.",
   },
 };
+
 const selectOneMoreFeeling = {
   element:
-    "html body div#root div.MuiContainer-root.MuiContainer-maxWidthXl.css-141bmmb div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.MuiCard-root.css-1jjdl4v div.MuiCardContent-root.css-1qw96cp div.MuiGrid-root.MuiGrid-container.css-1d3bbye div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-6.MuiGrid-grid-sm-4.MuiGrid-grid-md-3.MuiGrid-grid-lg-3.MuiGrid-grid-xl-2.css-yd7xg8",
+    "html body div#root div.MuiContainer-root.MuiContainer-maxWidthXl.css-141bmmb div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.MuiCard-root.css-1jjdl4v div.MuiCardContent-root.css-1qw96cp div.MuiGrid-root.MuiGrid-container.css-1d3bbye div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-6.MuiGrid-grid-sm-4.MuiGrid-grid-md-3.MuiGrid-grid-lg-3.MuiGrid-grid-xl-2.css-yd7xg8:nth-child(6)",
   popover: {
-    title: "Select one more",
-    description: "click on the feeling to select it.",
+    title: "Select one more feeling",
+    description:
+      "Click on this feeling to select it too.  Now, you have selected 2 feelings. Click the 'Next' button.",
   },
 };
 
